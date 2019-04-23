8ed6d554206786183d688575990629719c09affa
King Pra <kingi.pra@gmail.com>
2019-04-23 16:01:39 -0500

    added gulpfile and README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+1. to use gulpfile run this line in the terminal, under the project directory (project folder: minimalism).
+2. npm i gulp-newer gulp-imagemin browser-sync pump gulp-autoprefixer gulp-htmlmin gulp-cssnano gulp-uglify-es gulp-sass
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,97 @@
-function defaultTask(cb) {
+const gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  htmlmin = require("gulp-htmlmin"), //minify html
+  cssnano = require("gulp-cssnano"), //minify css
+  browserSync = require("browser-sync").create(), //live reload
+  imagemin = require("gulp-imagemin"), //minify images
+  uglify = require("gulp-uglify-es").default, //minify js
+  pump = require("pump"), // use in place of pipe method for better error handling
+  newer = require("gulp-newer"), //only updates updated files
+  autoprefixer = require("gulp-autoprefixer"); //auto adds prefixes for css
+// setup for live reload
+gulp.task("browserSync", () => {
+  browserSync.init({
+    server: {
+      baseDir: "docs/"
+    },
+    port: 3000
+  });
+});
+// live reload
+browserSyncReload = () => browserSync.reload({ stream: true });
 
-cb(); 
-}
+// older error handler, may not need this
+handleError = error => {
+  console.log(error.toString());
+  this.emit("end");
+};
 
-exports.default = defaultTask;
+gulp.task("default", ["html", "css", "js", "browserSync", "images"]);
+
+// HTML
+gulp.task("html", cb => {
+  pump(
+    [
+      gulp.src("index.html"),
+      newer("docs/index.html"),
+      htmlmin({ collapseWhitespace: true }),
+      gulp.dest("docs/"),
+      browserSyncReload()
+    ],
+    cb
+  );
+});
+
+//CSS
+gulp.task("css", cb => {
+  pump(
+    [
+      gulp.src("style.scss"),
+      //newer("docs/style.css"),
+      sass(),
+      cssnano({
+        autoprefixer: {
+          browsers: ["> 1%", "last 2 versions", "Firefox >= 20"],
+          add: true
+        }
+      }),
+      gulp.dest("docs/"),
+      browserSyncReload()
+    ],
+    cb
+  );
+});
+
+// JS
+gulp.task("js", cb => {
+  pump(
+    [
+      gulp.src("app.js"),
+      newer("docs/app.js"),
+      uglify(),
+      gulp.dest("docs/"),
+      browserSyncReload()
+    ],
+    cb
+  );
+});
+
+// Image Optimization
+gulp.task("images", cb => {
+  pump(
+    [
+      gulp.src("images/**.*"),
+      newer("docs/images/"),
+      imagemin(),
+      gulp.dest("docs/images/")
+    ],
+    cb
+  );
+});
+
+gulp.task("watch", ["default"], () => {
+  gulp.watch("index.html", ["html"]);
+  gulp.watch("**.scss", ["css"]);
+  gulp.watch("app.js", ["js"]);
+  gulp.watch("images/**.*", ["images"]);
+});
